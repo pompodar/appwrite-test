2a13247b00d9e839a9143c59f23267fe6b4da04a
User <user@Users-MacBook-Pro.local>
2024-10-27 20:25:44 +0200

    initial one

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { Login } from "./pages/Login";
+import { Home } from "./pages/Home";
+import { UserProvider, useUser } from "./lib/context/user";
+import { IdeasProvider } from "./lib/context/ideas";
+
+function App() {
+  const isLoginPage = window.location.pathname === "/login";
+
+  return (
+    <div>
+      <UserProvider>
+        <IdeasProvider>
+          <Navbar /> {/* Add the navbar before page content */}
+          <main>{isLoginPage ? <Login /> : <Home />}</main>
+         </IdeasProvider>
+      </UserProvider>
+    </div>
+  );
+}
+
+function Navbar() {
+  const user = useUser();
+
+  return (
+    <nav>
+      <a href="/">Idea tracker</a>
+      <div>
+        {user.current ? (
+          <>
+            <span>{user.current.email}</span>
+            <button type="button" onClick={() => user.logout()}>
+              Logout
+            </button>
+          </>
+        ) : (
+          <a href="/login">Login</a>
+        )}
+      </div>
+    </nav>
+  );
+}
+
+export default App;
---
+++ src/lib/appwrite.js
@@ -0,0 +1,9 @@
+import { Client, Databases, Account } from "appwrite";
+
+const client = new Client();
+client
+    .setEndpoint("https://cloud.appwrite.io/v1")
+    .setProject("67193f63002da27b2ff0");
+
+export const account = new Account(client);
+export const databases = new Databases(client);
